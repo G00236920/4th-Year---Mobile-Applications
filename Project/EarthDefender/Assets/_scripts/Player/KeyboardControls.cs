1916faabcdb733679e6cf0de359aa13fc0c2ed6b
Michael Kidd <G00236920@gmit.ie>
2018-09-30 12:11:39 +0100

    keyboard controls added

---
+++ Project/EarthDefender/Assets/_scripts/Player/KeyboardControls.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControls : MonoBehaviour {
+
+    private PlayerPhysics Phys;
+
+    // Use this for initialization
+    void Start () {
+
+        Phys = GetComponent<PlayerPhysics>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Phys.MoveForward();
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Phys.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Phys.MoveLeft();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Phys.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Phys.MoveBackward();
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Phys.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Phys.MoveRight();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Phys.Stop();
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            
+        }
+
+    }
+}
---
+++ Project/EarthDefender/Assets/_scripts/SceneSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
