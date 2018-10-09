48c87c13dfb2bd3cd64d7cb9e197f8a9d49390f9
Michael Kidd <G00236920@gmit.ie>
2018-10-09 21:50:53 +0100

    DockSpawner added

---
+++ Project/EarthDefender/Assets/_scripts/DockSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockSpawner : MonoBehaviour {
+
+	private const string ENEMY_PARENT_NAME = "Enemies";
+	private const string SPAWN_METHOD = "Spawn";
+
+	[SerializeField]
+	[Header("Number of Points")]
+	private Transform[] waypoints;
+	[SerializeField]
+	private float spawnDelay = .2f;
+	[SerializeField]
+	private float spawnInterval = 1.3f;
+	[SerializeField]
+	private float enemyStartSpeed = .2f;
+	[SerializeField]
+	private GameObject enemPrefab;
+	private GameObject enemyParent;
+
+	private void Start(){
+
+		if(!enemyParent){
+			enemyParent = new GameObject(ENEMY_PARENT_NAME);
+		}
+
+		SpawnRepeating();
+	
+	}
+
+	private void SpawnRepeating(){
+
+		InvokeRepeating(SPAWN_METHOD, spawnDelay, spawnInterval);
+
+	}
+
+	private void Spawn(){
+
+		var enemy = Instantiate(enemPrefab, enemyParent.transform);
+		enemy.transform.position = transform.position;
+
+		var follower = enemy.GetComponent<WaypointFollower>();
+		follower.Speed = enemyStartSpeed;
+
+		foreach(var waypoint in waypoints){
+
+			follower.AddPointsToFollow(waypoint.position);
+
+		}
+
+	}
+
+
+}
---
+++ Project/EarthDefender/Assets/_scripts/WaypointFollower.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour {
+
+private IList<Vector3> waypoints = new List<Vector3>();
+private Vector3 currentWaypoint;
+private float speed;
+private Rigidbody rb;
+
+public float Speed { get { return speed; } set { speed = value; } }
+
+	private void Start(){
+
+		rb = GetComponent<Rigidbody>();
+
+		NextPointToFollow();
+
+	}
+
+	private void FixedUpdate(){
+
+		if(HasMorePoints()) {
+
+			Move();
+
+		}
+
+	}
+
+	private void NextPointToFollow(){
+
+		if(HasMorePoints()) {
+
+			currentWaypoint = waypoints.First();
+
+		}
+
+	}
+
+	private void Move(){
+
+		rb.position = Vector3.MoveTowards(rb.position, currentWaypoint, speed * Time.deltaTime);
+
+		if(Vector3.Distance(rb.position, currentWaypoint) < 0.01){
+
+			rb.position = new Vector2(currentWaypoint.x, currentWaypoint.y);
+
+			waypoints.Remove(currentWaypoint);
+
+			if(HasMorePoints()) {
+
+				NextPointToFollow();
+
+			}
+
+		}
+
+	}
+
+	private bool HasMorePoints(){
+
+		return waypoints.Count != 0;
+
+	}
+
+	public void AddPointsToFollow(Vector3 point){
+
+		waypoints.Add(point);
+
+	}
+
+}
