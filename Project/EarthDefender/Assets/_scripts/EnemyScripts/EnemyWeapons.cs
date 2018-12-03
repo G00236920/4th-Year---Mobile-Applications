230b82f35080ef5873e4a1030ea035e1284ac9da
Michael Kidd <G00236920@gmit.ie>
2018-12-03 21:32:10 +0000

    Added multipl backgrounds, added boss multidirectional fire

--- Project/EarthDefender/Assets/_scripts/EnemyScripts/EnemyWeapons.cs
+++ Project/EarthDefender/Assets/_scripts/EnemyScripts/EnemyWeapons.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyWeapons : MonoBehaviour {
 	
 	public GameObject phaser;
+    [SerializeField]
+    private bool MultiFire = false;
 
 	// Update is called once per frame
 	void Start(){
@@ -25,6 +27,25 @@ public class EnemyWeapons : MonoBehaviour {
         x = this.gameObject.transform.GetChild(0).position.x + .2f;
 
         Instantiate(phaser, new Vector3(x,y,z), new Quaternion(0f,0f,180f,0f));
+
+        if(MultiFire)
+        {
+
+            x = this.gameObject.transform.GetChild(0).position.x - .4f;
+            y = this.gameObject.transform.GetChild(0).position.y - 1f;
+            z = this.gameObject.transform.GetChild(0).position.z;
+
+            var torpedo = Instantiate(phaser, new Vector3(x,y,z), new Quaternion(0f,0f,180f,0f));
+
+            torpedo.transform.Rotate(0, 0, -45);
+
+            x = this.gameObject.transform.GetChild(0).position.x + .4f;
+
+            torpedo = Instantiate(phaser, new Vector3(x,y,z), new Quaternion(0f,0f,180f,0f));
+
+            torpedo.transform.Rotate(0, 0, 45);
+
+        }
 
     }
 
