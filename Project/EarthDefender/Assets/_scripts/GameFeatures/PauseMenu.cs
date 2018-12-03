4111680d41c1a05047027272a1cef7872c0db624
Michael Kidd <G00236920@gmit.ie>
2018-12-03 22:48:35 +0000

    Added Pause Menu to levels

---
+++ Project/EarthDefender/Assets/_scripts/GameFeatures/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject inGameMenu;
+	public GameObject Overlay;
+	private bool paused = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+
+			if(paused)
+			{
+				unPauseGame();
+			}
+			else
+			{
+				pauseGame();
+			}
+
+		}
+	}
+
+	public void pauseGame(){
+
+		Time.timeScale = 0;
+
+		paused = true;
+		inGameMenu.SetActive(true);
+		Overlay.SetActive(false);
+		
+	}
+
+	void unPauseGame(){
+
+		Time.timeScale = 1;
+
+		paused = false;
+		inGameMenu.SetActive(false);
+		Overlay.SetActive(true);
+
+	}
+}
