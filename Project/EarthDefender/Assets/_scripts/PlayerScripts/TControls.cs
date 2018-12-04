fecfbb0eca8fcb37aad33053302f3761e563deee
Michael Kidd <G00236920@gmit.ie>
2018-12-04 13:32:58 +0000

    fixing touch screen controls

--- Project/EarthDefender/Assets/_scripts/PlayerScripts/TControls.cs
+++ Project/EarthDefender/Assets/_scripts/PlayerScripts/TControls.cs
@@ -18,50 +18,25 @@ public class TControls : MonoBehaviour {
 		
 	}
 
-	void FixedUpdate(){
-		Vector3 v;
+	void Update(){
 
-		if (Input.touchCount != 1) {
-			dragging = false; 
-			return;
-        }
+		for(var touch : Touch in Input.touches)
+		{
 
-
-		Touch touch = Input.touches[0];
-		Vector3 pos = touch.position;
-
-        if(touch.phase == TouchPhase.Began) {
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(pos);
-
-			if(Physics.Raycast(ray, out hit) && (hit.collider.tag == "Draggable"))
+			if(touch.phase == TouchPhase.Began)
 			{
-				toDrag = hit.transform;
-				
-				dist = hit.transform.position.z - Camera.main.transform.position.z;
-				
-				v = new Vector3(pos.x, pos.y, dist);
-				v = Camera.main.ScreenToWorldPoint(v);
 
-				offset = toDrag.position - v;
+				var ray = Camera.main.ScreenPointToRay(touch.position);
 
-				dragging = true;
+				if(Physics.RayCast(ray)){
+
+					Debug.Log("test");
+
+				}
+
 			}
 
-        }
-		if (dragging && touch.phase == TouchPhase.Moved) {
-
-			v = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-			v = Camera.main.ScreenToWorldPoint(v);
-			toDrag.position = v + offset;
-
 		}
-		if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
-
-			dragging = false;
-			
-		}
-
 
 	}
 	
